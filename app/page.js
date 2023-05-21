fcf0709567d275b406dae00a95ad50fef39fbf05
Bernard Onuh <onuhbernard4@gmail.com>
2023-05-21 12:53:14 +0100

    polygonzkEvm

--- app/page.js
+++ app/page.js
@@ -1,44 +1,60 @@
+"use client";
+import { ThirdwebProvider, ConnectWallet, Web3Button, useAddress  } from "@thirdweb-dev/react";
 import Image from 'next/image'
 
 export default function Home() {
+  const address = '0x0D9BEfD32156D129A1F68F9f431D7B99E5bBD915';
+  const address1 = useAddress()
+  const quantity = '1';
   return (
+    <ThirdwebProvider activeChain="mumbai">
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        {/*<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.js</code>
-        </p>
+        </p>*/}
+        <ConnectWallet/>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
+          {/*<a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            By{' '}
+          ></a>*/}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
+              src="/matic.png"
+              alt="Matic Logo"
+              className="px-3"
+              width={50}
+              height={10}
               priority
             />
-          </a>
+          
+          Polygon Guild Akure
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] rounded:bd "
+          src="/Bernard.jpg"
+          alt="Bernard Profile "
           width={180}
           height={37}
           priority
         />
+        
       </div>
-
+{address1}
+      <Web3Button
+      contractAddress="0x8F1bcB432FA3876455A77CBbd49B4f1eBeACc88d"
+      action={(contract) => {
+        contract.call("claim", [address, quantity])
+      }}
+    >
+      MINT
+    </Web3Button>
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -47,13 +63,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Opensea{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+           Let us sell our NFT, How much can we sell .
           </p>
         </a>
 
@@ -109,5 +125,6 @@ export default function Home() {
         </a>
       </div>
     </main>
+    </ThirdwebProvider>
   )
 }
